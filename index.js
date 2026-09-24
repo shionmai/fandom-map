f23618a9502ea3c162cff3588e40732f72e03115
maimai <NobeWaye@gmail.com>
2026-09-24 00:45:22 -0700

    Sorts fandoms alphabetically and allows searches
    Added search functionality and sorting for fandoms.

--- index.js
+++ index.js
@@ -1,43 +1,36 @@
 console.log("hello :D");
 const pinContainer = document.getElementById("pin-container");
 const searchContainer = document.getElementById("search-content")
+const searchInput = document.getElementById("search-input")
 function makePin(name, locationTop, locationLeft, fandomLabels) {
     const newPin = document.createElement('div');
     newPin.className = "pin";
     newPin.id = `pin-${name}`;
     newPin.style.top = String(locationTop) + "%";
     newPin.style.left = String(locationLeft) + "%";
-
     const newPinShape = document.createElement('div');
     newPinShape.className = "pin-shape";
     newPin.appendChild(newPinShape);
-
     const newPinInfo = document.createElement('div');
     newPin.appendChild(newPinInfo);
     newPinInfo.className = "pin-info";
     const newPinInfoSpan = document.createElement('span');
     newPinInfo.appendChild(newPinInfoSpan);
     newPinInfoSpan.textContent = String(name);
-
     for(const label of fandomLabels){
-
         const labelInSearch = document.getElementById(label);
-
         labelInSearch.addEventListener('mouseenter', () => {
             newPin.classList.add('pin-active');
             newPinShape.classList.add('pin-shape-active');
             newPinInfo.style.display = "flex";
         });
-
         labelInSearch.addEventListener('mouseleave', () => {
             newPin.classList.remove('pin-active');
             newPinShape.classList.remove('pin-shape-active');
             newPinInfo.style.display = "";
         });
     }
-
     pinContainer.appendChild(newPin);
-
 }
 function makeSearchContent(name) {
     const newFandom = document.createElement('div');
@@ -45,6 +38,15 @@ function makeSearchContent(name) {
     newFandom.id = name;
     newFandom.textContent = name;
     searchContainer.appendChild(newFandom);
+}
+function sortSearch(){
+    const divArray = Array.from(searchContainer.children);
+    divArray.sort((a,b) => { 
+        return a.textContent.trim().localeCompare(b.textContent.trim());
+    });
+    divArray.forEach(item => {
+        searchContainer.appendChild(item);
+    });
 }
 async function loadFandomData() {
     try {
@@ -56,18 +58,31 @@ async function loadFandomData() {
         for(const fandom of data.fandoms){
             let label = "";
             let fandomLabels = []
-
             for(const name of fandom.fandomName){
                 label += name + "\n";
                 fandomLabels.push(name);
                 makeSearchContent(name);
             }
-
             makePin(label, fandom.location[0], fandom.location[1], fandomLabels);
         }
+        sortSearch();
     }
     catch (error) {
         console.error("Could not fetch the JSON file:", error);
     }
 }
 loadFandomData();
+function search(query){
+    query = query.toLowerCase().trim();
+    for(const item of searchContainer.children){
+        const itemText = item.textContent.toLowerCase();
+        if(itemText.includes(query)){
+            item.style.display = "";
+        } else {
+            item.style.display = "none";
+        }
+    }
+}
+searchInput.addEventListener("input", function(event) {
+    search(event.target.value)
+})
